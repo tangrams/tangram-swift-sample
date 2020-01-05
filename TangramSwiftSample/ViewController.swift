aecfb69d6d7986de9a472cdb6ccaac8b03b9959f
Matt Blair <blair1618@gmail.com>
2020-01-05 17:18:34 -0500

    Implement ViewController with Tangram Map instance

--- TangramSwiftSample/ViewController.swift
+++ TangramSwiftSample/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import TangramMap
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, TGMapViewDelegate, TGRecognizerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let mapView = self.view as! TGMapView
+
+        mapView.mapViewDelegate = self
+        mapView.gestureDelegate = self
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
+        let apiKey = Bundle.main.infoDictionary?["NextzenApiKey"] as! String
+
+        let sceneUpdates = [TGSceneUpdate(path: "global.sdk_api_key", value: apiKey)]
+
+        let mapView = self.view as! TGMapView
+
+        let sceneUrl = URL(string: "https://www.nextzen.org/carto/bubble-wrap-style/9/bubble-wrap-style.zip")!
+
+        mapView.loadSceneAsync(from: sceneUrl, with: sceneUpdates)
+    }
+
+    func mapViewDidCompleteLoading(_ mapView: TGMapView) {
+        print("MapView did complete loading")
+
+        let newYork = CLLocationCoordinate2DMake(40.70532700869127, -74.00976419448854)
+
+        mapView.cameraPosition = TGCameraPosition(center: newYork, zoom: 15, bearing: 0, pitch: 0)
+    }
+
+    func mapView(_ view: TGMapView!, recognizer: UIGestureRecognizer!, didRecognizeSingleTapGesture location: CGPoint) {
+        print("Tap gesture at location: \(location.x), \(location.y)")
+    }
 }
 
